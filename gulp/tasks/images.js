35058e9ce7cd81118540b130b24f72367772a490
Alexandr Patsura <alexandrpatsura@gmail.com>
2019-11-04 11:23:16 +0200

    add image format webp

--- gulp/tasks/images.js
+++ gulp/tasks/images.js
@@ -1,27 +1,23 @@
 // images
-const imagemin       = require('gulp-imagemin');
-const imageminWebp   = require('imagemin-webp');
+// const webp = require('gulp-webp');
 
 module.exports = function () {
   $.gulp.task('images', function () {
     return $.gulp.src($.paths.images.src) // {since: $.glp.gulp.lastRun('images')})
     .pipe($.glp.imagemin([
-      $.glp.imagemin.gifsicle({ interlaced: true }),
+      // $.glp.imagemin.gifsicle({ interlaced: true }),
       $.glp.imagemin.jpegtran({ progressive: true }),
       $.glp.imagemin.optipng({ optimizationLevel: 5 }),
-      $.glp.imagemin.svgo({
-        plugins: [
-          { removeViewBox: true },
-          { cleanupIDs: false }
-        ]
-      })
+      // $.glp.imagemin.svgo({
+      //   plugins: [
+      //     { removeViewBox: true },
+      //     { cleanupIDs: false }
+      //   ]
+      // })
     ], {
       verbose: true
     }))
-    .pipe($.gulp.dest($.paths.images.dest), {
-      use: [
-        imageminWebp({ quality: 50 })
-      ]
-    });
+    .pipe($.glp.webp({ quality: 85 }))
+    .pipe($.gulp.dest($.paths.images.dest));
   });
 }
